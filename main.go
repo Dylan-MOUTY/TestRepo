1e4825fe0ed152309a24231ad56ba247e33b3fa0
Dylan-MOUTY <dylanmouty77@gmail.com>
2026-02-11 12:15:58 +0100

    test

--- main.go
+++ main.go
@@ -13,11 +13,16 @@ import (
 func main() {
 	port := flag.String("port", ":8080", "Port du serveur")
 	flag.Parse()
-	if err := api.LoadData(); err != nil {
-		log.Fatal(err)
-	}
+
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
 	go func() { <-sig; os.Exit(0) }()
+
+	go func() {
+		if err := api.LoadData(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
 	log.Fatal(server.Start(*port))
 }
